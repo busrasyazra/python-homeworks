a4e0a22f82706f0fdc3cc74d5df42b282e00e573
busrasyazra <81545300+busrasyazra@users.noreply.github.com>
2021-07-03 12:47:32 -0700

    homework

--- Numbers_to_Roman_numerals.py
+++ Numbers_to_Roman_numerals.py
@@ -1,4 +1,4 @@
-def Convert_Roman( num):
+ def Convert_Roman( num):
         symbol = [ "M", "CM", "D", "CD","C", "XC", "L", "XL","X", "IX", "V", "IV","I"]
         value = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
         roman_num = ''
